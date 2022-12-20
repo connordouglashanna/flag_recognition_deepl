6c3b4f95faed8410a608fa0d892efb515111fb3f
connordouglashanna <connordouglashanna@gmail.com>
2022-12-20 17:41:45 -0600

    data added
    Data added to the folder. Processing pending.

---
+++ flag_recognition.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 20 15:21:10 2022
+
+@author: condo
+"""
+
+# baby's first independent deepl project
+
+# for image processing I know the following three libraries are related but
+# I'm not sure which does what... 
+
+# pillow
+# skimage
+# numpy.ndarrays
+
