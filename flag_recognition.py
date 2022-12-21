17efe18b521c4b2b74745100dd6cf9e76468e576
connordouglashanna <connordouglashanna@gmail.com>
2022-12-20 19:39:52 -0600

    added notes for data processing
    notes for data processing. mostly package names/steps/stuff Reid told me to look up.

--- flag_recognition.py
+++ flag_recognition.py
@@ -10,7 +10,20 @@ Created on Tue Dec 20 15:21:10 2022
 # for image processing I know the following three libraries are related but
 # I'm not sure which does what... 
 
+## protip from Reid: 
+    # pytorch dataset class
+
 # pillow
 # skimage
 # numpy.ndarrays
 
+## steps for each:
+
+# crop to specified aspect ratio
+
+# shrink to appropriate size for nn
+# use pillow resize() method
+
+# normalize images
+
+# dimensionality reduction?
